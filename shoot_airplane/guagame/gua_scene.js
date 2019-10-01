83ebe7b85dc4173c5ba7f0b3d2cce230766b8991
emean <870112002@qq.com>
2019-10-02 01:28:26 +0800

    lession 8

--- shoot_airplane/guagame/gua_scene.js
+++ shoot_airplane/guagame/gua_scene.js
@@ -1,6 +1,7 @@
 class GuaScene {
     constructor(game) {
         this.game = game
+        this.debugModeEnabled = true
         this.elements = []
     }
     static new(game) {
@@ -8,6 +9,7 @@ class GuaScene {
         return i
     }
     addElement(img) {
+        img.scene = this
         this.elements.push(img)
     }
 
@@ -18,6 +20,15 @@ class GuaScene {
         }
     }
     update() {
-
+        if (this.debugModeEnabled) {
+            for (var i = 0; i < this.elements.length; i++) {
+                var e =this.elements[i]
+                e.debug && e.debug()
+            }
+        }
+        for (var i = 0; i < this.elements.length; i++) {
+            var e = this.elements[i]
+            e.update()
+        }
     }
 }
--- shoot_airplane/scene/main/scene.js
+++ shoot_airplane/scene/main/scene.js
@@ -1,10 +1,49 @@
+const config = {
+    palyer_speed: 10,
+    cloud_speed: 1,
+    enemy_speed: 5,
+    bullet_speed: 5,
+    fire_cooldown: 9,
+}
+
+class Bullet extends GuaImage{
+    constructor(game) {
+        super(game, 'bullet')
+        this.setup()
+    }
+    setup() {
+        this.speed = config.bullet_speed
+    }
+    update() {
+        this.y -= this.speed
+    }
+}
+
 class Player extends GuaImage {
     constructor(game) {
         super(game, 'player')
-        this.speed = 10
+        this.setup()
+    }
+    setup() {
+        this.speed = 5
+        this.cooldown = 0
     }
     update() {
-
+        this.speed = config.palyer_speed
+        if (this.cooldown > 0) {
+            this.cooldown--
+        }
+    }
+    fire() {
+        if (this.cooldown == 0) {
+            this.cooldown = config.fire_cooldown
+            var x = this.x + this.w / 2
+            var y = this.y
+            var b = Bullet.new(this.game)
+            b.x = x
+            b.y = y
+            this.scene.addElement(b)
+        }
     }
     moveLeft() {
         this.x -= this.speed
@@ -20,26 +59,100 @@ class Player extends GuaImage {
     }
 }
 
+class Enemy extends GuaImage {
+    constructor(game) {
+        var type = randomBetween(0, 4)
+        var name = 'enemy' + type
+        super(game, name)
+        this.setup()
+    }
+    setup() {
+        this.speed = randomBetween(2, 5)
+        this.x = randomBetween(0, 350)
+        this.y = -randomBetween(0, 200)
+    }
+    update() {
+        this.y += this.speed
+        if (this.y > 600) {
+            this.setup()
+        }
+    }
+}
+
+class Cloud extends GuaImage {
+    constructor(game) {
+        super(game, 'cloud')
+        this.setup()
+    }
+    setup() {
+        this.speed = 1
+        this.x = randomBetween(0, 350)
+        this.y = -randomBetween(0, 200)
+    }
+    update() {
+        this.y += this.speed
+        if (this.y > 600) {
+            this.setup()
+        }
+    }
+    debug() {
+        this.speed = config.cloud_speed
+    }
+}
+
 class Scene extends GuaScene {
     constructor(game) {
         super(game)
         this.setup()
+        this.setupInputs()
     }
     setup() {
         var game = this.game
+        this.numberOfEnemies = 10
         this.bg = GuaImage.new(game, 'sky')
-        this.cloud = GuaImage.new(game, 'cloud')
+        this.cloud = Cloud.new(game, 'cloud')
 
         // this.player = GuaImage.new(game, 'player')
         this.player = Player.new(game)
         this.player.x = 100
         this.player.y = 150
-        
+
         this.addElement(this.bg)
         this.addElement(this.cloud)
         this.addElement(this.player)
+        //
+        this.addEnemies()
+    }
+    addEnemies() {
+        var es = []
+        for (var i = 0; i < this.numberOfEnemies; i++) {
+            var e = Enemy.new(this.game)
+            es.push(e)
+            this.addElement(e)
+        }
+        this.enemies = es
+    }
+    setupInputs() {
+        var g = this.game
+        var s = this
+        g.registerAction('a', function(){
+            s.player.moveLeft()
+        })
+        g.registerAction('d', function(){
+            s.player.moveRight()
+        })
+        g.registerAction('w', function(){
+            s.player.moveUp()
+        })
+        g.registerAction('s', function(){
+            s.player.moveDown()
+        })
+        g.registerAction('j', function(){
+            s.player.fire()
+        })
     }
     update() {
+        super.update()
         this.cloud.y += 1
     }
     // draw() {
@@ -48,95 +161,3 @@ class Scene extends GuaScene {
         // this.game.drawImage(this.player)
     // }
 }
-
-// var Scene = function(game) {
-//     var s = {
-//         game: game,
-//     }
-//     // 初始化
-//     var paddle = Paddle(game)
-//     var ball = Ball(game)
-//
-//     var score = 0
-//
-//     var blocks = loadLevel(game, 1)
-//
-//     game.registerAction('a', function(){
-//         paddle.moveLeft()
-//     })
-//     game.registerAction('d', function(){
-//         paddle.moveRight()
-//     })
-//     game.registerAction('f', function(){
-//         ball.fire()
-//     })
-//
-//     s.draw = function() {
-//         // draw 背景
-//         game.context.fillStyle = "#554"
-//         game.context.fillRect(0, 0, 400, 300)
-//         //draw
-//         game.drawImage(paddle)
-//         game.drawImage(ball)
-//         for (var i = 0; i < blocks.length; i++) {
-//             var block = blocks[i]
-//             if (block.alive) {
-//                 game.drawImage(block)
-//             }
-//         }
-//         // draw Labels
-//         game.context.fillText('分数： ' + score, 10, 290)
-//     }
-//     s.update = function() {
-//         ball.move()
-//         // 判断游戏结束
-//         if (ball.y > paddle.y) {
-//             // 跳转到游戏结束的场景
-//             var end = SceneEnd.new(game)
-//             game.replaceScene(end)
-//
-//         }
-//         // 判断相撞
-//         if (paddle.collide(ball)) {
-//             ball.fantan()
-//         }
-//         // 判断 ball 和 block 相撞
-//         for (var i = 0; i < blocks.length; i++) {
-//             var block = blocks[i]
-//             if (block.collide(ball)) {
-//                 log('block 相撞')
-//                 block.kill()
-//                 ball.fantan()
-//                 score += 100
-//             }
-//         }
-//     }
-//
-//     // mouse event
-//     enableDrag = false
-//     game.canvas.addEventListener('mousedown', function(event) {
-//         var x = event.offsetX
-//         var y = event.offsetY
-//         log(x, y)
-//         // 检查是否点中 ball
-//         if (ball.hasPoint(x, y)) {
-//             // 设置拖拽状态
-//             enableDrag = true
-//         }
-//     })
-//     game.canvas.addEventListener('mousemove', function(event) {
-//         var x = event.offsetX
-//         var y = event.offsetY
-//         if (enableDrag) {
-//             ball.x = x
-//             ball.y = y
-//         }
-//     })
-//     game.canvas.addEventListener('mouseup', function(event) {
-//         var x = event.offsetX
-//         var y = event.offsetY
-//         enableDrag = false
-//     })
-//
-//     return s
-// }
