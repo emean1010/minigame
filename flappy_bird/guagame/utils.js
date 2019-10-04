fdfb25294f393b51a760b1c59848fca54fa5303f
emean <870112002@qq.com>
2019-10-04 23:05:13 +0800

    flappybird 1

---
+++ flappy_bird/guagame/utils.js
@@ -0,0 +1,26 @@
+// var e = sel => document.querySelector(sel)
+//
+// var log = function(s) {
+//     e('#id-text-log').value += '\n' + s
+// }
+var log = console.log.bind(console)
+
+var imageFromPath = function(path) {
+    var img = new Image()
+    img.src = path
+    return img
+}
+
+var rectIntersects = function(a, b) {
+    if (b.y > a.y && b.y < a.y + a.image.height) {
+        if (b.x > a.x && b.x < a.x + a.image.width) {
+            return true
+        }
+    }
+    return false
+}
+
+const randomBetween = function(start, end) {
+    var n = Math.random() * (end - start + 1)
+    return Math.floor(n + start)
+}
---
+++ flappy_bird/main.js
@@ -0,0 +1,90 @@
+var loadLevel = function(game, n) {
+    n = n - 1
+    var level = levels[n]
+    var blocks = []
+    for (var i = 0; i < level.length; i++) {
+        var p = level[i]
+        var b = Block(game, p)
+        blocks.push(b)
+    }
+    return blocks
+}
+
+var enableDebugModel = function(game, enable) {
+    if (!enable) {
+        return
+    }
+    window.paused = false
+    window.addEventListener('keydown', function(event) {
+        var k = event.key
+        if (k == 'p') {
+            window.paused = !window.paused
+        } else if ('1234567'.includes(k)){
+            blocks = loadLevel(game, Number(k))
+        }
+    })
+    document.querySelector('#id-input-speed').addEventListener('input', function(event) {
+        var input = event.target
+        window.fps = Number(input.value)
+    })
+}
+
+var __main = function() {
+
+    var images = {
+        bullet: 'img/bullet.png',
+        cloud: 'img/cloud.png',
+        player: 'img/player.png',
+        sky: 'img/sky.png',
+        enemy0: 'img/enemy0.png',
+        enemy1: 'img/enemy1.png',
+        enemy2: 'img/enemy2.png',
+        enemy3: 'img/enemy3.png',
+        enemy4: 'img/enemy4.png',
+        fire: 'img/fire.png',
+        // 走路动画
+        // w1: 'img/walking/w1.png',
+        // w2: 'img/walking/w2.png',
+        // w3: 'img/walking/w3.png',
+        // w4: 'img/walking/w4.png',
+        // w5: 'img/walking/w5.png',
+        // w6: 'img/walking/w6.png',
+        // w7: 'img/walking/w7.png',
+        // w8: 'img/walking/w8.png',
+        // w9: 'img/walking/w9.png',
+        // 闲置
+        idle1: 'img/player-idle/player-idle-1.png',
+        idle2: 'img/player-idle/player-idle-2.png',
+        idle3: 'img/player-idle/player-idle-3.png',
+        idle4: 'img/player-idle/player-idle-4.png',
+        // 跑动
+        run1: 'img/player-run/player-run-1.png',
+        run2: 'img/player-run/player-run-2.png',
+        run3: 'img/player-run/player-run-3.png',
+        run4: 'img/player-run/player-run-4.png',
+        run5: 'img/player-run/player-run-5.png',
+        run6: 'img/player-run/player-run-6.png',
+        run7: 'img/player-run/player-run-7.png',
+        run8: 'img/player-run/player-run-8.png',
+        run9: 'img/player-run/player-run-9.png',
+        run10: 'img/player-run/player-run-10.png',
+        //
+        cave: 'img/cave.png',
+        // flappy bird
+        bg: 'bird/bg.png',
+        ground: 'bird/ground.png',
+        b1: 'bird/b1.png',
+        b2: 'bird/b2.png',
+        b3: 'bird/b3.png',
+    }
+    var game = GuaGame.instance(30, images, function(g) {
+        // var s = Scene.new(g)
+        var s = SceneTitle.new(g)
+        g.runWithScene(s)
+    })
+
+    enableDebugModel(game, true)
+
+}
+
+__main()
