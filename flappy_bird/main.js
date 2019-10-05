ac16a77b333c6501dfeee2be159170b9f202c9e2
emean <870112002@qq.com>
2019-10-05 22:48:08 +0800

    flappy bird 完

--- flappy_bird/main.js
+++ flappy_bird/main.js
@@ -32,46 +32,9 @@ var enableDebugModel = function(game, enable) {
 var __main = function() {
 
     var images = {
-        bullet: 'img/bullet.png',
-        cloud: 'img/cloud.png',
-        player: 'img/player.png',
-        sky: 'img/sky.png',
-        enemy0: 'img/enemy0.png',
-        enemy1: 'img/enemy1.png',
-        enemy2: 'img/enemy2.png',
-        enemy3: 'img/enemy3.png',
-        enemy4: 'img/enemy4.png',
-        fire: 'img/fire.png',
-        // 走路动画
-        // w1: 'img/walking/w1.png',
-        // w2: 'img/walking/w2.png',
-        // w3: 'img/walking/w3.png',
-        // w4: 'img/walking/w4.png',
-        // w5: 'img/walking/w5.png',
-        // w6: 'img/walking/w6.png',
-        // w7: 'img/walking/w7.png',
-        // w8: 'img/walking/w8.png',
-        // w9: 'img/walking/w9.png',
-        // 闲置
-        idle1: 'img/player-idle/player-idle-1.png',
-        idle2: 'img/player-idle/player-idle-2.png',
-        idle3: 'img/player-idle/player-idle-3.png',
-        idle4: 'img/player-idle/player-idle-4.png',
-        // 跑动
-        run1: 'img/player-run/player-run-1.png',
-        run2: 'img/player-run/player-run-2.png',
-        run3: 'img/player-run/player-run-3.png',
-        run4: 'img/player-run/player-run-4.png',
-        run5: 'img/player-run/player-run-5.png',
-        run6: 'img/player-run/player-run-6.png',
-        run7: 'img/player-run/player-run-7.png',
-        run8: 'img/player-run/player-run-8.png',
-        run9: 'img/player-run/player-run-9.png',
-        run10: 'img/player-run/player-run-10.png',
-        //
-        cave: 'img/cave.png',
         // flappy bird
         bg: 'bird/bg.png',
+        pipe: 'bird/pipe.png',
         ground: 'bird/ground.png',
         b1: 'bird/b1.png',
         b2: 'bird/b2.png',
--- flappy_bird/scene/title/scene_title.js
+++ flappy_bird/scene/title/scene_title.js
@@ -1,3 +1,68 @@
+class Pipes {
+    constructor(game) {
+        this.game = game
+        this.pipes = []
+        this.pipeSpace = 150
+        this.管子横向间距 = 200
+        this.columsOfPipe = 3
+        for (var i = 0; i < this.columsOfPipe; i++) {
+            var p1 = GuaImage.new(game, 'pipe')
+            p1.flipY = true
+            p1.x = 500 + i * this.管子横向间距
+            var p2 = GuaImage.new(game, 'pipe')
+            p2.x = p1.x
+            this.resetPipePosition(p1, p2)
+            this.pipes.push(p1)
+            this.pipes.push(p2)
+        }
+    }
+    static new(game) {
+        return new this(game)
+    }
+    resetPipePosition(p1, p2) {
+        p1.y = randomBetween(-200, 0)
+        p2.y = p1.y + p1.h + this.pipeSpace
+    }
+    debug() {
+        this.pipeSpace = config.pipe_space.value
+        this.管子横向间距 = config.管子横向间距.value
+    }
+    update() {
+        for (var i = 0; i < this.pipes.length; i += 2) {
+            var p1 = this.pipes[i]
+            var p2 = this.pipes[i + 1]
+            p1.x -= 5
+            p2.x -= 5
+            if (p1.x < -100) {
+                p1.x += this.管子横向间距 * this.columsOfPipe
+            }
+            if (p2.x < -100) {
+                p2.x += this.管子横向间距 * this.columsOfPipe
+                this.resetPipePosition(p1, p2)
+            }
+        }
+    }
+    draw() {
+        var context = this.game.context
+        for (var p of this.pipes) {
+            context.save()
+
+            var w2 = p.w / 2
+            var h2 = p.h / 2
+            context.translate(p.x + w2, p.y + h2)
+            var scaleX = p.flipX ? -1 : 1
+            var scaleY = p.flipY ? -1 : 1
+            context.scale(-scaleX, scaleY)
+            context.rotate(p.rotation * Math.PI / 180)
+            context.translate(-w2, -h2)
+
+            context.drawImage(p.texture, 0, 0)
+
+            context.restore()
+        }
+    }
+}
+
 class SceneTitle extends GuaScene {
     constructor(game) {
         super(game)
@@ -7,6 +72,9 @@ class SceneTitle extends GuaScene {
         // cave bg
         var bg = GuaImage.new(game, 'bg')
         this.addElement(bg)
+        // 加入水管
+        this.pipe = Pipes.new(game)
+        this.addElement(this.pipe)
         // 循环移动的地面
         this.grounds = []
         for (var i = 0; i < 30; i++) {
@@ -18,6 +86,7 @@ class SceneTitle extends GuaScene {
         }
         this.skipCount = 5
         // bird
+        this.birdSpeed = 2
         var b = GuaAnimation.new(game)
         b.x = 180
         b.y = 200
@@ -25,6 +94,9 @@ class SceneTitle extends GuaScene {
         this.addElement(b)
 
         this.setupInputs()
+    }
+    debug() {
+        this.birdSpeed = config.bird_spped.value
     }
     update() {
         super.update()
@@ -44,10 +116,10 @@ class SceneTitle extends GuaScene {
         var self = this
         var b = this.bird
         self.game.registerAction('a', function(keyStatus) {
-            b.move(-2, keyStatus)
+            b.move(-self.birdSpeed, keyStatus)
         })
         self.game.registerAction('d', function(keyStatus) {
-            b.move(2, keyStatus)
+            b.move(self.birdSpeed, keyStatus)
         })
         self.game.registerAction('j', function(keyStatus) {
             b.jump()
